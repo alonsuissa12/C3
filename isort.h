3936ac97003a8eed4c14db1ef932fe36952b4ac2
aviya oren <avoaviya@gmail.com>
2022-12-22 11:33:33 +0200

    isort h

--- isort.h
+++ isort.h
@@ -5,4 +5,28 @@
 #ifndef HW3_ISORT_H
 #define HW3_ISORT_H
 
+/*
+*this function gets :
+* 1.pointer to a place in array 
+* 2.number 
+* the function will move each of the i next organs
+* one place to the right.  
+*if i=0 just switch arr[i+1] , arr[i] 
+*
+*/
+void shift_element(int* arr, int i);
+
+/*
+*this function gets:
+* array size [len]
+* the function will sort the array from small to big.
+*
+*/
+void insertion_sort(int* arr , int len);
+
+/*
+* function to print the array that the pointer is point on
+*/
+void print_array(int *data, int n);
+
 #endif //HW3_ISORT_H
--- txtfind.c
+++ txtfind.c
@@ -3,3 +3,4 @@
 //
 
 #include "txtfind.h"
+
